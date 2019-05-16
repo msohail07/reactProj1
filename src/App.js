6f6ea561953eb65541923c1d49dd31a4c285b7df
Mustafa Sohail <mustafasohail07@gmail.com>
2019-05-16 15:09:00 -0500

    Finished basic functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
     state = {
-        characters : [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-              },
-              {
-                name: 'Mac',
-                job: 'Bouncer',
-              },
-              {
-                name: 'Dee',
-                job: 'Aspring actress',
-              },
-              {
-                name: 'Dennis',
-                job: 'Bartender',
-              }
-        ]
+        characters : []
     }
 
     removeCharacter = (index) => {
@@ -33,29 +17,15 @@ class App extends Component {
         })
     }
 
-    render() {
-        // const characters = [
-        //     {
-        //       name: 'Charlie',
-        //       job: 'Janitor',
-        //     },
-        //     {
-        //       name: 'Mac',
-        //       job: 'Bouncer',
-        //     },
-        //     {
-        //       name: 'Dee',
-        //       job: 'Aspring actress',
-        //     },
-        //     {
-        //       name: 'Dennis',
-        //       job: 'Bartender',
-        //     },
-        // ]
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+    }
 
+    render() {
         return (
             <div className='container'>
                 <Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         );
     }
